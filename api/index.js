f0b9a09dd12bb90d66006d13d41ef91261018d41
Sofia Costa <>
2020-07-26 22:49:39 +0100

    Server running

---
+++ api/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const { env: { PORT = 8080, NODE_ENV: env }, argv: [, , port = PORT] } = process
+
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
+const winston = require('winston')
+const morgan = require('morgan')
+const { name, version } = require('./package')
+
+const app = express()
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+const logger = winston.createLogger({
+    level: env === 'development' ? 'debug' : 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'server.log' })
+    ]
+})
+
+if (env !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }))
+}
+
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.use(cors())
+
+app.listen(port, () => logger.info(`server ${name} ${version} up and running on port ${port}`))
+
+process.on('SIGINT', () => {
+    logger.info('server abruptly stopped')
+
+    process.exit(0)
+})
