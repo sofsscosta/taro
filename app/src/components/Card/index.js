ed7532122e3601deb75dcb45afcfba3217e1fe11
Sofia Costa <>
2020-10-07 16:11:29 +0200

    Cards with back

--- app/src/components/Card/index.js
+++ app/src/components/Card/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cardsBack from '../../assets/images/cards_back.png';
 
-const Card = ({ image }) => <img className="card" alt="card" src={require(`../../../public/${image}`)} />;
+const Card = ({ image, selectedCard, onClick }) => (
+  <img
+    onClick={() => {
+      console.log(image);
+      onClick(image);
+    }}
+    className="card"
+    alt="card"
+    src={selectedCard === image ? require(`../../assets/images/${image}`) : cardsBack}
+  />
+);
 
 Card.propTypes = {
   image: PropTypes.string.isRequired,
+  selectedCard: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Card;
--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { getCards } from '../../logic';
+import getCards from '../../logic';
 import Home from './Home';
 
 const HomeContainer = () => {
   const [data, setData] = useState();
+  const [selectedCard, setSelectedCard] = useState()
 
   useEffect(
     () =>
       (async () => {
         const cards = await getCards();
+        console.log(cards)
         setData(cards);
       })(),
     []
   );
 
-  return <Home cards={data} />;
+  const selectCard = card => {
+    setSelectedCard(card)
+  }
+
+  return <Home cards={data} onClick={selectCard} selectedCard={selectedCard}/>;
 };
 
 export default HomeContainer;
